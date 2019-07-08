71603b692a8855890893ace5c94fc1c9a1202a61
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-07-08 19:10:10 +0200

    more

---
+++ README.md
@@ -0,0 +1,27 @@
+# Space Gfx
+
+## What is this?
+It's a game where you pilot a ship through _spaaaaace_. There isn't much to do
+just yet. There are planets around that just kinda sit there.
+
+## What is there to see?
+The universe is just a bunch of planets. The space is in 2D, but stretches
+"infinitely" in all directions. Using a sigmoid projection, you can see it 
+all at once, as if its wrapped over a globe.
+
+## Trajectory
+The main appeal of the game is to mess with your trajectory. At all times there
+is a little pink stripe from your ship into the abyss. It predicts your
+motion in the next seconds / minutes. It tends to curl and bend with the
+pull of the planets. You can bump your trajectory by tapping or holding WASD.
+
+## Messing with params
+The projection is somewhat complicated. In simple terms its a sigmoid function:
+```
+fn(x) = 2 / (1 + e^(-x)) - 1 
+```
+This has the effect of wrapping an infinite x domain onto a bounded y domain.
+however, you can mess with a few of these parameters. You can scale and shift x,
+which has the effect of warping and pinching space around your ship. These
+variables, along with the length of your trajectory can be manipulated by
+selecting a variable with numbers 1-6, and scrolling the mousewheel.
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate ggez;
+use ggez::graphics::Vector2;
+
 use core::f32::consts::E;
 use ggez::event::Keycode;
 use ggez::event::Mod;
@@ -16,11 +18,34 @@ const PINK: Color = Color {
     b: 0.5,
     a: 1.0,
 };
+const PINK2: Color = Color {
+    r: 0.9,
+    g: 0.3,
+    b: 0.3,
+    a: 1.0,
+};
+const PINK3: Color = Color {
+    r: 0.6,
+    g: 0.2,
+    b: 0.2,
+    a: 1.0,
+};
+const PINK4: Color = Color {
+    r: 0.4,
+    g: 0.12,
+    b: 0.12,
+    a: 1.0,
+};
 
 struct Me {
     data: Instancedata,
     velocity: Point2,
 }
+
+
+const ACCEL: f32 = 0.0004;
+const TRAJ_PTS_CAP: usize = 3000;
+const TRAJ_SCALING: usize = 30;
 struct MainState {
     me: Me,
     planets: Vec<Instancedata>,
@@ -31,9 +56,10 @@ struct MainState {
     y_pressed: Option<bool>,
     scrolling_index: usize,
     scrolling_text: Vec<Option<ggez::graphics::Text>>,
+    trajectory: [Point2; TRAJ_PTS_CAP],
 }
 
-const NUM_VARS: usize = 5;
+const NUM_VARS: usize = 6;
 
 #[derive(Debug, Copy, Clone)]
 struct Instancedata {
@@ -48,8 +74,24 @@ struct SigConfig {
 impl Default for SigConfig {
     fn default() -> Self {
         Self {
-            x: [0.04, -2.0, 1., 1., 1.],
+            x: [0.02, -1.0, 1., 0.3, 1., 200.],
         }
+    }
+}
+impl SigConfig {
+    fn sig(&self, dist: f32) -> f32 {
+        let q = 2. / (1.0 + E.powf(-self.x[1])) - 1.;
+        let divisor = 1.0 + E.powf(-self.x[1] - dist * self.x[0]);
+        let intermediate = 2. / divisor - 1. - q;
+        intermediate / (1. - q)
+    }
+    fn project(&self, origin: Point2, pt: Point2, window_dims: Point2) -> (Point2, f32) {
+        let rel: Point2 = pt - origin.coords;
+        let dist = length(rel);
+        let sigged = self.sig(dist);
+        let newdist: f32 = sigged * 250.0 * self.x[4];
+        let projected = rel / dist * newdist + (window_dims.coords / 2.0);
+        (projected, sigged)
     }
 }
 
@@ -58,40 +100,37 @@ fn length(p: Point2) -> f32 {
 }
 
 impl MainState {
-    fn sig(&self, dist: f32) -> f32 {
-        let x: &[f32; NUM_VARS] = &self.sig_config.x;
-
-        let q = 2. / (1.0 + E.powf(-x[1])) - 1.;
-        // println!("q {}", q);
-
-        (2. / (
-            1.0 + E.powf(-x[1] - dist * x[0])
-            // higher mult pushes more to edges
-        ) - 1.
-            - q)
-            / (1. - q)
-
-        // 2. / (
-        // 	1.0 + E.powf(-dist*0.08)
-        // ) - 1.
-
-        // (2. / (
-        // 	1.0 + E.powf(-2. - dist*a) // higher mult pushes more to edges
-        // ) - 1.4621172-0.29947686) / 0.23840594
+    /// given a point in space, returns a vector representing its acceleration
+    fn grav_accel<'a>(planets: impl Iterator<Item=&'a Instancedata>, pt: Point2) -> Vector2 {
+        let mut v = Vector2::new(0., 0.);
+        for p in planets {
+            let impulse = p.pos - pt.coords;
+            let len = length(impulse) + 3.0;
+            let new_len = 1.0 / (len*len);
+            v += impulse.coords / len * new_len * (p.scale*2.);
+        }
+        v * 0.3
     }
     fn new(ctx: &mut Context) -> GameResult<MainState> {
-        let mut planets = vec![];
-        const SQN: usize = 10;
-        let mut rng = rand::rngs::StdRng::from_seed([0; 32]);
-        let mut clos =
-            || rng.sample::<f32, _>(Standard).powf(3.0) * 600.0 * if rng.gen() { -1. } else { 1. };
+        let mut planets: Vec<Instancedata> = vec![];
+        const SQN: usize = 5;
+        use rand::rngs::StdRng;
+        let mut rng = StdRng::from_seed([0; 32]);
+        let rng = &mut rng;
+        let clos =
+            |rng: &mut StdRng| rng.sample::<f32, _>(Standard).powf(3.0) * 900.0 * if rng.gen() { -1. } else { 1. };
 
         for _i in 0..SQN {
             for _j in 0..SQN {
-                let dp = Instancedata {
-                    pos: Point2::new(clos(), clos()),
-                    scale: 0.4,
+                let colliding =
+                    |pt1: Point2| planets.iter().any(|pt2| length(pt1 - pt2.pos.coords) < 60.);
+                let pos = loop {
+                    let pos = Point2::new(clos(rng), clos(rng));
+                    if !colliding(pos) {
+                        break pos;
+                    }
                 };
+                let dp = Instancedata { pos, scale: rng.gen::<f32>() + 0.1 };
                 planets.push(dp);
             }
         }
@@ -108,6 +147,7 @@ impl MainState {
             velocity: Point2::new(0., 0.),
         };
         let s = MainState {
+            trajectory: [Point2::new(0., 0.); TRAJ_PTS_CAP],
             me,
             planets,
             planet_sprite,
@@ -128,13 +168,12 @@ trait Joystick: Copy {
     fn val(self) -> f32;
 }
 
-const ACCEL: f32 = 0.01;
 impl Joystick for Option<bool> {
     fn val(self) -> f32 {
         match self {
-            Some(false) => -ACCEL,
+            Some(false) => ACCEL,
             None => 0.0,
-            Some(true) => ACCEL,
+            Some(true) => -ACCEL,
         }
     }
     fn bump_left(&mut self) {
@@ -153,14 +192,41 @@ impl Joystick for Option<bool> {
     }
 }
 
+impl MainState {
+    
+}
+
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        // 1. compute keypress acceleration
         let mut acc = Point2::new(self.x_pressed.val(), self.y_pressed.val());
         if self.x_pressed.is_some() && self.y_pressed.is_some() {
             acc /= 1.4;
         }
+
+        // 2. update velocity (keypress + gravity)
+        self.me.velocity += acc.coords + Self::grav_accel(self.planets.iter(), self.me.data.pos);
+
+        // 3. update position
         self.me.data.pos += self.me.velocity.coords;
-        self.me.velocity += acc.coords;
+
+        {
+            let mut prev_ = self.me.data.pos;
+            let mut vel_ = self.me.velocity;
+            self.trajectory[0] = self.window_dims * 0.5;
+            let tu = (self.sig_config.x[5] as usize).min(TRAJ_PTS_CAP).max(8);
+            for p in self.trajectory.iter_mut().skip(1).take(tu) {
+                for _ in 0..TRAJ_SCALING {
+                    // 1. reuse keypress acceleration
+                    // 2. update velocity (keypress + gravity)
+                    vel_ += acc.coords + Self::grav_accel(self.planets.iter(), prev_);
+
+                    // 3. update position
+                    prev_ += vel_.coords;
+                }
+                *p = self.sig_config.project(prev_, self.me.data.pos, self.window_dims).0;
+            } 
+        }
         Ok(())
     }
 
@@ -171,14 +237,7 @@ impl event::EventHandler for MainState {
 
         ggez::graphics::set_color(ctx, WHITE)?;
         for i in self.planets.iter() {
-            let rel: Point2 = i.pos - self.me.data.pos.coords;
-            let dist = length(rel);
-            //    let sig = 2.0 / (
-            // 	1.0 + E.powf(-dist*0.08)
-            // ) - 1.0;
-            let sigged = self.sig(dist);
-            let newdist: f32 = sigged * 250.0 * x[4];
-            let dest = rel / dist * newdist + (self.window_dims.coords / 2.0);
+            let (dest, sigged) = self.sig_config.project(i.pos, self.me.data.pos, self.window_dims);
             let s = (i.scale * x[3] * (1.0 - sigged).powf(x[2])).max(0.005);
             let dp = DrawParam {
                 dest,
@@ -213,6 +272,16 @@ impl event::EventHandler for MainState {
             ggez::graphics::draw_ex(ctx, f, dp)?;
         }
 
+        let tu = (self.sig_config.x[5] as usize).min(TRAJ_PTS_CAP).max(8);
+        ggez::graphics::set_color(ctx, PINK)?;
+        graphics::line(ctx, &self.trajectory[0..=tu/4], 1.0)?;
+        ggez::graphics::set_color(ctx, PINK2)?;
+        graphics::line(ctx, &self.trajectory[tu*1/4..=tu*2/4], 1.0)?;
+        ggez::graphics::set_color(ctx, PINK3)?;
+        graphics::line(ctx, &self.trajectory[tu*2/4..=tu*3/4], 1.0)?;
+        ggez::graphics::set_color(ctx, PINK4)?;
+        graphics::line(ctx, &self.trajectory[tu*3/4..tu], 1.0)?;
+
         graphics::present(ctx);
         Ok(())
     }
@@ -236,11 +305,16 @@ impl event::EventHandler for MainState {
             Keycode::D => self.x_pressed.bump_right(),
             Keycode::W => self.y_pressed.bump_left(),
             Keycode::S => self.y_pressed.bump_right(),
+            Keycode::F4 => {
+                let q = ggez::graphics::is_fullscreen(ctx);
+                ggez::graphics::set_fullscreen(ctx, !q).unwrap()
+            },
             Keycode::Num1 => self.scrolling_index = 0,
             Keycode::Num2 => self.scrolling_index = 1,
             Keycode::Num3 => self.scrolling_index = 2,
             Keycode::Num4 => self.scrolling_index = 3,
             Keycode::Num5 => self.scrolling_index = 4,
+            Keycode::Num6 => self.scrolling_index = 5,
             _ => (),
         }
         assert!(self.scrolling_index < NUM_VARS);
@@ -262,16 +336,18 @@ pub fn main() {
         "Welcome to space! Fly around and play with your view projection\n\
          --------------------------------------\n\
          use WASD to accelerate your viewpoint through space.\n\
+         use ESCAPE to exit\n\
          use SPACE to come to a stop\n\
          use BACKSPACE to jump to (0,0)\n\
          use number keys 1,2,3... to select a variable\n\
-         use mousewheel to scale selected variable up and down\n"
+         use mousewheel to scale selected variable up and down\n\
+         --------------------------------------\n\
+         For example, try: (0.09, -3, 1.8, 3.9, 1)\n\
+         and (0.01, -1, 1.8, 0.3, 1.11)\n"
     );
     let c = conf::Conf::new();
     let mut ctx = &mut Context::load_from_conf("super_simple", "ggez", c).unwrap();
     let state = &mut MainState::new(ctx).unwrap();
-
-    // println!("sig {:?} to {:?} (should be 0.0 to 1.0)", state.sig(0.0), state.sig(999999999.0));
     ggez::graphics::set_background_color(&mut ctx, BLACK);
     event::run(ctx, state).unwrap();
 }
